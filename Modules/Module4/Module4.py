9a6f6b63b26901f9140942e958b2a06f8680ea23
CSI-Lucas-Garcia <24-031@sanignacio.pr>
2021-09-14 14:54:21 -0400

    contestacion preguntas

--- Modules/Module4/Module4.py
+++ Modules/Module4/Module4.py
@@ -1,12 +1,27 @@
-print("What did you have for Breakfast?")
-meal = "toast"
-print(meal)
-print("What was your Lunch?")
-meal = "rice and beans"
-print(meal)
-print("Have you had dinner?")
-meal = "pizza"
-print(meal)
+# print("What did you have for Breakfast?")
+# meal = "toast"
+# print(meal)
+# print("What was your Lunch?")
+# meal = "rice and beans"
+# print(meal)
+# print("Have you had dinner?")
+# meal = "pizza"
+# print(meal)
 
->>>2bacon = 20
->>>print('2acon + 1')
+
+# mynumber = 360
+# print(f"The number is {mynumber}")
+
+# numberofballs = 7
+# print(f"I played volleyball with {numberofballs} balls")
+
+# s0 = "What is my length? Store it as a number in a variable."
+# ss0 = len(s0)
+# print(ss0)
+
+s1 = "Hello Student! My name is Carlos Jose Cobián. I enjoy coding and video games!"
+ss1 = (s1[22:44:])
+print(ss1)
+
+SS1 = ss1.upper()
+print(SS1)
