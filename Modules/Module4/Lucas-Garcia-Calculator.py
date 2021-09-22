cea21c1c42ef9efa3b21193d805dcab66c346eb7
CSI-Lucas-Garcia <24-031@sanignacio.pr>
2021-09-21 20:22:01 -0400

    Create Lucas-Garcia-Calculator.py
    code de math

---
+++ Modules/Module4/Lucas-Garcia-Calculator.py
@@ -0,0 +1,26 @@
+# print("Hello World")
+# print("What is your name?: ")
+# myName = input()
+# print("Nice to meet you", myName)
+# print("How old are you?")
+# grade = input()
+# print("You will be " + str(int(grade) + 2) + " in 2 years")
+
+print("Calculate area of Circle")
+print("""
+   CCCCCCCC
+ CCC      CCC
+CC          CC
+CC          CC
+CC          CC
+ CCC      CCC
+   CCCCCCCC   
+   """)
+print("What is the formula to calculate the area of a circle?")
+print("A = pi * radius**2, is the formula to calculate the area of a circle")
+pi = float(input("What is pi?: "))
+radius = int(input("what is the radius**2?: "))
+print("The area of the circle is: ", pi * radius**2)
+
+
+
