f662549b64f9fc83c05d301a30de0a24679b52be
CSI-Lucas-Garcia <24-031@sanignacio.pr>
2021-09-27 20:58:23 -0400

    Create Tarea.py
    Work

---
+++ Modules/Module4/Tarea.py
@@ -0,0 +1,32 @@
+# print("Calculate Celcius to Farenheit")
+# Celcius = int(input("what is the Celcius?: "))
+# print("The degree form Celcius to Farenheit is: ", Celcius * 1.8+32)
+
+# print("Calculate the average")
+# variable1 = int(input("first variable: "))
+# variable2 = int(input("second variable: "))
+# variable3 = int(input("third variable: "))
+# print("The sum of the number is: ", variable1 + variable2 + variable3)
+# sum = variable1 + variable2 + variable3 
+# print("The average is: ", sum / 3)
+
+print("Welcome to the Garcia concert")
+print("The cost for each seat is: close view = $100, medium view = $60, far view = $20")
+
+close_view = int(input("How many tickets sold for close view ="))
+medium_view = int(input("How many tickets sold for medium view ="))
+far_view = int(input("How many tickets sold for far view ="))
+
+def seats(close_view, medium_view, far_view):
+    print(f"The cost for close view tickets is: {int(close_view) * 100}")
+    print(f"The cost for medium view tickets is: {int(medium_view) * 60}")
+    print(f"The cost for far view tickets is: {int(far_view) * 20}")
+print()
+seats(close_view, medium_view, far_view)
+Sales_CV = close_view * 95
+Sales_MV = medium_view * 60 
+Sales_FV= far_view * 20 
+def TicketsTotal(Sales_CV, Sales_MV, Sales_FV):
+    print(f"The total of tickets sales is: ${int(Sales_CV) + int(Sales_MV) + int(Sales_FV)}")
+print()
+TicketsTotal(Sales_CV, Sales_MV, Sales_FV)
