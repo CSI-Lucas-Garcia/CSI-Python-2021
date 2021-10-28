d04c2ad5307c3860e7ce906e270967a382fa6219
CSI-Lucas-Garcia <24-031@sanignacio.pr>
2021-10-28 14:32:08 -0400

    Project

---
+++ Projects/Projectile-Motion/Lucas-Garcia-Projectile-Motion.py
@@ -0,0 +1,18 @@
+import math 
+#  gun = "M4A1"
+# cartridge = "5.56X45mm"
+# ammunition = "FMJ"
+# velocity = 957
+
+# Building = "Aquablue1"
+# BuildingHeight = 85
+
+# Gravity = 9.81
+
+def projectilefunction(gun:str, catridge:str, ammunition:str, velocity:int, Building:str, BuildingHeight:int, Gravity:int):
+    time_s = math.sqrt((2 * BuildingHeight) / Gravity) 
+    Distance = (velocity * time_s)
+
+    print(f"The gun i selected was {gun}. The {gun} has a catridge of {cartridge}. The {gun} is a {ammunition} weapon. The rate of fire of the {gun} is {velocity}. The buildin i selected is {Building}. It has a height of {BuildingHeight}.\n")
+
+projectilefunction("M4A1", "5.56X45", "FMJ", 957, "Aquablue1", 85, 9.81)
