1fc91dfed60e7965bd0c4b3b8c49e99027c6e970
CSI-Lucas-Garcia <24-031@sanignacio.pr>
2021-10-29 14:16:30 -0400

    Update Lucas-Garcia-Projectile-Motion.py

--- Projects/Projectile-Motion/Lucas-Garcia-Projectile-Motion.py
+++ Projects/Projectile-Motion/Lucas-Garcia-Projectile-Motion.py
@@ -9,10 +9,18 @@ import math
 
 # Gravity = 9.81
 
-def projectilefunction(gun:str, catridge:str, ammunition:str, velocity:int, Building:str, BuildingHeight:int, Gravity:int):
-    time_s = math.sqrt((2 * BuildingHeight) / Gravity) 
-    Distance = (velocity * time_s)
+# def projectilefunction(gun:str, catridge:str, ammunition:str, velocity:int, Building:str, BuildingHeight:int, Gravity:int):
+#     time_s = math.sqrt((2 * BuildingHeight) / Gravity) 
+#     Distance = (velocity * time_s)
 
-    print(f"The gun i selected was {gun}. The {gun} has a catridge of {cartridge}. The {gun} is a {ammunition} weapon. The rate of fire of the {gun} is {velocity}. The buildin i selected is {Building}. It has a height of {BuildingHeight}.\n")
+#     print(f"The gun i selected was {gun}. The {gun} has a catridge of {cartridge}. The {gun} is a {ammunition} weapon. The rate of fire of the {gun} is {velocity}. The buildin i selected is {Building}. It has a height of {BuildingHeight}.\n")
 
-projectilefunction("M4A1", "5.56X45", "FMJ", 957, "Aquablue1", 85, 9.81)
+# projectilefunction("M4A1", "5.56X45", "FMJ", 957, "Aquablue1", 85, 9.81)
+
+x = 2
+if x = 2:
+   print("x is equal to two.")
+elif x > 2 or x <= 5:
+   print("x is greater than two.")
+else:
+   print("x is less than two.")
