c6d0108b1bf83e2c9e0a421674b7d8115f811afb
CSI-Lucas-Garcia <24-031@sanignacio.pr>
2022-01-28 15:02:07 -0400

    Update Lucas-Garcia-Hangman.py

--- Projects/Lucas-Garcia-Hangman.py
+++ Projects/Lucas-Garcia-Hangman.py
@@ -1,3 +1,5 @@
+from ctypes.wintypes import WORD
+from curses.ascii import isalpha
 import random 
 word_list = ["Santo", "Loyola","Ignacio","Servicioso","Honesto","Iglesia","Jerusalen","Hombre de armas","respeto","Sacerdote","Jesuita"]
 
@@ -7,7 +9,7 @@ def get_word(word_list):
     return word.upper()
 
 def play(word):
-    word_completion = "_" * len(word)
+    word_completion = "-" * len(word)
     guessed = False
     guessed_letters = []
     guessed_words = []
@@ -18,20 +20,45 @@ def play(word):
     print(word_completion)
     print("\n")
     while not guessed and tries > 0:
-        guess = input("Adivina una palabra o letra: ").upper()
+        guess = input("Adivina la palabra: ").upper()
         if len(guess) == 1 and guess.isalpha():
-            print("You already tried", guess, "!")
-        elif guess not in word:
-            print(guess, "Isn't in the word :(")     
-            tries -=1
-            guessed_letters.append(guess)
+            if guess in guessed_letters:
+                print("Ya trataste eso", guess, "!")
+            elif guess not in word:
+                print(guess, "No es la palabra :(")     
+                tries -=1
+                guessed_letters.append(guess)
+            else:
+                print("Lo hiciste", guess, "esta en la palabra!")
+                guessed_letters.append(guess)
+                word_as_list = list(word_completion)
+                indices = [i for i, letter in enumerate(word) if letter == guess]
+                for index in indices:
+                    word_as_list[index] = guess 
+                word_completion = "".join(word_as_list)
+                if "-" not in word_completion:
+                    guessed = True
+        elif len(guess) == len(word) and guess.isalpha():
+            if guessed in guessed_words:
+                print("Ya trataste eso", guess, "!")
+            elif guess != word:
+                print(guess, "No es la palabra : (")
+                tries -= 1
+                guessed_words.append(guess)
+            else:
+                guessed = True
+                word_completion = word
         else:
-            print("You did it", guess, "is in the word!")
-            guessed_letters.append(guess)
+            print("Invalid input")
+        print(display_hangman(tries))
+        print(word_completion)
+        print("\n")
 
-
-
-
+    if guessed:
+        print("Buen trabajo, pegaste la palabra!!! :)")
+    else: print("Lo siento, pero te quedaste sin tries. La palabra era", + WORD + "Para la prixima vez!")
+                
+    
 
 
 
@@ -115,3 +142,13 @@ def display_hangman(tries):
                             
 
     ]
+    return stages[tries]
+def main():
+    word = get_word
+    play(word)
+    while input("De nuevo? (Y/N)").upper() == "Y":
+        word = get_word(word_list)
+        play(word)
+
+if __name__ == "__main__":
+    main() 
